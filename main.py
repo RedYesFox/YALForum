9aaea81768b7a6b875aa5c17a5450f5ac64e4130
RedYesFox <nicksemenikhin2008@yandex.ru>
2025-03-15 10:11:22 +0700

    master

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('base.html', title=title)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
